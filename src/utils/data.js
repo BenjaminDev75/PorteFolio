b4134fef5a1d7a64148c6d5200c3ed996959ac98
Benjamin Pereira <159250572+BenjaminDev75@users.noreply.github.com>
2024-10-13 13:07:12 +0200

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,45 @@
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "frontend-icon.png",
+    skills: [
+      { skill: "HTML5", percentage: "50%" },
+      { skill: "CSS3", percentage: "50%" },
+      { skill: "JavaScript", percentage: "10%" },
+      { skill: "React.js", percentage: "20%" },
+      { skill: "AngularJS", percentage: "40%" }
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "backend-icon.png",
+    skills: [
+      { skill: "Python", percentage: "40%" },
+      { skill: "SQL", percentage: "30%" },
+      { skill: "Java", percentage: "20%" },
+      { skill: "PHP", percentage: "10%" },
+      { skill: "Low Code", percentage: "30%" },
+    ],
+  },
+  {
+    title: "Logiciels",
+    icon: "tools-icon.png",
+    skills: [
+      { skill: "Git & GitHub", percentage: "20%" },
+      { skill: "Visual Studio Code", percentage: "60%" },
+      { skill: "JetBrain Tools", percentage: "30%" },
+      { skill: "Magic xpa", percentage: "40%" },
+      { skill: "Microsoft SQL Serveur Management Studio 18", percentage: "35%" },
+    ],
+  },
+  {
+    title: "Qualités",
+    icon: "soft-skills-icon.png",
+    skills: [
+      { skill: "Débogage", percentage: "75%" },
+      { skill: "Autodidacte", percentage: "60%" },
+      { skill: "Travail en groupe", percentage: "70%" },
+      { skill: "Minutieux", percentage: "90%" },
+    ],
+  },
+];
