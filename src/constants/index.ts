0c7e168812d1f4d71a8dda773184dbb0d9792a86
Benjamin Pereira <159250572+BenjaminDev75@users.noreply.github.com>
2024-10-13 13:06:22 +0200

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,153 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/discord.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/instagram.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Java",
+      Image: "/java.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Magic xpa",
+      Image: "/magic.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Python",
+      Image: "/python.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "PHP",
+      Image: "/php.png",
+      width: 70,
+      height: 70,
+    },
+  ];
+
+
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "PHP Storm",
+      Image: "/phpstorm.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Git",
+      Image: "/git.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "GitHub",
+      Image: "/gitwhite.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+
+
+
+
+export const Backend_skill2 = [
+  {
+    skill_name: "MySQL",
+    Image: "/mysql.png",
+    width: 70,
+    height: 70,
+  },
+];
