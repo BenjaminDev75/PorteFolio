250190792e831c95342bd432b9d7b46ce305ac2a
BenjaminDev75 <benjaminpereira7515@gmail.com>
2024-12-01 20:26:45 +0100

    Maj Projet : Ajout des PowerPoint + aujustement             des explication

--- src/app/components/Projet/ProjectsSection.jsx
+++ src/app/components/Projet/ProjectsSection.jsx
@@ -17,7 +17,7 @@ const projectsData = [
   {
     id: 2,
     title: "Computech",
-    description: "Site static de vente d'ordinateur et accessoire gaming",
+    description: "Site static de vente d'ordinateur et accessoire gaming développé en HTML/CSS dans le cadre d'un projet scolaire en 1ère année de BTS",
     image: "/images/projects/7.jpg",
     tag: ["All", "Web","Ecole"],
     gitUrl: "https://github.com/BenjaminDev75/Projet-HTML",
@@ -26,8 +26,7 @@ const projectsData = [
   {
     id: 3,
     title: "Doc Installation GLPI sous Linux",
-    description: "Fichier PDF d'une documentation pour l'installation de GLPI sous l'environnement Linux avec Ubuntu" +
-        "dans une VirtualBox",
+    description: "Fichier PDF d'une documentation pour l'installation de GLPI sous l'environnement Linux avec Ubuntu avec VirtualBox",
     image: "/images/projects/6.jpg",
     tag: ["All", "Ecole"],
     gitUrl: "/images/Projet_GLPI.pdf",
@@ -36,7 +35,8 @@ const projectsData = [
   {
     id: 4,
     title: "Ricosheep",
-    description: "Projet de jeu labyrinthe ou l'objectif est de placer chaques moutons sur les hautes herbes, ce projet étais un projet de groupe réaliser pour le second semestre de Licence Maths-Informatique",
+    description: "Projet de jeu labyrinthe ou l'objectif est de placer chaques moutons sur les hautes herbes avec un système de niveau, du niveau le plus simple au plus compliqué. Il est aussi possible d'avoir la solution si"
+                  +" on ne trouve pas la solution et aussi de pouvoir reset la partie en cours. Ce projet étais un projet de groupe réalisé pour le second semestre de la 1ère année de ma Licence Mathématiques-Informatique",
     image: "/images/projects/5.jpg",
     tag: ["All", "Jeux","Ecole"],
     gitUrl: "https://github.com/BenjaminDev75/Ricosheep",
@@ -45,8 +45,8 @@ const projectsData = [
   {
     id: 5,
     title: "Jeu Snake",
-    description: "Projet de jeu Snake en C dans le but etais de modifier les regles du jeu en modifant les règles du jeu : mettre en place un système de vitesses, "+
-                    "faire en sorte d'afficher 3 fruit à la fois, et de faire manger 3 fruit au serpent pour grandir d'un taille",
+    description: "Projet de programmation du jeu Snake en language C, dont le but étais de modifier les règles du jeu à partir d'un code trouvé sur internet : mettre en place un système de vitesses, "+
+                    "faire en sorte d'afficher 3 fruits à la fois, et de faire manger 3 fruits au serpent pour le faire grandir d'une taille",
     image: "/images/projects/8.jpg",
     tag: ["All", "Jeux","Ecole"],
     gitUrl: "https://github.com/BenjaminDev75/Snake-C",
@@ -54,11 +54,31 @@ const projectsData = [
   },
   {
     id: 6,
-    title: "Developpement d'une application web d'un Planning",
+    title: "Cosmétté",
+    description: "Site de vente de produits de beauté naturels et bio, fait avec Shopify avec un systeme de paiement Paypal ainsi que la gestion d'avis pour chaque produit du site. Inclue la recherche"+
+                    " de produit par catégorie. Site créé dans le cadre d'un projet scolaire fait lors de ma 2ème année de BTS.",
+    image: "/images/projects/9.png",
+    tag: ["All", "Ecole","Web"],
+    gitUrl: "/images/Shopify.pdf",
+    previewUrl: "/images/Projet_Shopify.mp4",
+  },
+  {
+    id: 7,
+    title: "Gestion d'une Association",
+    description: "Site web développé avec PHP/Bootstrap, dont le but étais d'utilisé l'IA pour la création du code ainsi que des images avec une gestion de droit et de connexion au site par email et mot de passe"
+                    +" pour la suppréssion l'ajout ou la modification d'un personnel, d'un projet, d'un don, et d'un pays. Chat-GPT, Blackbox, Microsoft-Copilot et GitHub-Copilot on été les IA utilisé lors de ce projet",
+    image: "/images/projects/10.png",
+    tag: ["All", "Ecole","Web"],
+    gitUrl: "/images/Shopify.pdf",
+    previewUrl: "/images/Gestion_Asso.pdf",
+  },
+  {
+    id:8,
+    title: "Developpement d'un planning",
     description: "Planning destiner au chargé de planning pour la gestion des tournées pour chaque chauffeur, avec une vu sur les tournées de la semaine, "+
-    "pour chaque chauffeur, avec la possibilité de modifier les tournées, de les supprimer d'un jour, de les ajouter et de les déplacer avec un drag and drop"+
-    "le tout developper avec Logiciel Magic XPA et AngularJS",
-    image: "/images/projects/sui.jpg",
+                    "pour chaque chauffeur, avec la possibilité de modifier les tournées, de les supprimer d'un jour, de les ajouter et de les déplacer avec un drag and drop"+
+                      "le tout developper avec Logiciel Magic XPA et AngularJS",
+    image: "/images/projects/11.png",
     tag: ["All", "Pro"],
     gitUrl: "/",
     previewUrl: "/",
@@ -146,6 +166,7 @@ const ProjectsSection = () => {
               description={project.description}
               imgUrl={project.image}
               gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
             />
           </motion.li>
         ))}
